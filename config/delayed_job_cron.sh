ee40f6e31817705faf9b542f8548cd891b2f7ec8
sergio marchesini <sergiomarchesini@gmail.com>
2020-03-25 19:35:04 +0100

    change delayed job settings

--- config/delayed_job_cron.sh
+++ config/delayed_job_cron.sh
@@ -2,7 +2,7 @@
 
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-APP_PATH="/home/rails/vcv-decidim"
+APP_PATH="/home/rails/vcv/decidim/current"
 
 if ! [ -s $APP_PATH/tmp/pids/delayed_job.pid ]; then
   RAILS_ENV=production $APP_PATH/bin/delayed_job start
